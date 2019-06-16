692140c572b0b2cf39ccc2a28815068293ad7327
wangyue1994 <240498767@qq.com>
2019-06-16 12:43:10 +0800

    'wy'

--- gitchange.js
+++ gitchange.js
@@ -3,22 +3,29 @@ var exec = require('child_process').exec;
 
 var now = new Date().setDate(16);
 var old = new Date().setDate(10);
-while (now > old) {
-console.log(now > old)
-now = now - 86400000 * parseInt(Math.random()*5, 10);
-fs.writeFile('./README.md', 'd' + now , (err) => {
-    if (err)
-        throw err;
-    var iosdate = new Date(now);
-    exec('git add .', () => {
-        console.log('add')
-        exec(`git commit -am 'wy' --date="${iosdate}"`, () => {
-            console.log('commit')
-            exec('git push', () => {
-                console.log(iosdate);
-                // 最多相隔5天提交一次
+
+function gitchange() {
+    if (now > old) {
+        console.log(now > old);
+        fs.appendFile('./README.md', 'd' + now , (err) => {
+            if (err)
+                throw err;
+            var iosdate = new Date(now);
+            console.log(iosdate);
+            exec('git add .', () => {
+                console.log('add')
+                exec(`git commit -am 'wy' --date="${iosdate}"`, () => {
+                    console.log('commit')
+                    exec('git push', () => {
+                        console.log('push');
+                        // 最多相隔5天提交一次
+                        now = now - 86400000 * parseInt(Math.random()*5, 10);
+                        gitchange();
+                    });
+                });
             });
-        });
-    });
-})
+        })
+    }
 }
+
+gitchange();
